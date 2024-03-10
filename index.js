43b5f4a25b16b3a95ea8abdb26668d511f02f69a
ibenge <benjamin.uforo@gmail.com>
2024-03-10 14:11:32 +0100

    chore: add svg desktop and phone

--- index.js
+++ index.js
@@ -2,52 +2,122 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
-  console.log("here");
 
   const text = new SplitType(".split");
   const text2 = new SplitType(".split2");
   const text3 = new SplitType(".split3");
   const shapeFuture = new SplitType(".shapeFuture");
 
-  const tl = gsap.timeline();
 
-  tl.to(text.chars, {
-    y: 0,
-    stagger: 0.1,
-    opacity:1
-    
-  })
-    .to(text2.words, {
+  gsap.fromTo(
+    text.chars,
+    {
+      opacity: 0,
+      y: 50,
+    },
+    {
+      opacity: 1,
       y: 0,
       stagger: 0.1,
-      
-    })
-    // .to(text3.chars, {
-    //   y: 0,
-    //   stagger: 0.1,
-      
-    // })
-    .to(shapeFuture.chars, {
-      y: 0,
-      stagger: 0.1,
-      
-    })
-    .to("#graph-icon", {
-      scale: 1,
-    })
-    .to("#comp-image", {
-      scale: 1,
-    })
-    .to("#music-icon", {
-      scale: 1,
-    });
+    }
+  )
+    gsap.fromTo(
+      text2.chars,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        opacity: 1,
+        stagger: 0.1,
+        y: 0,
+      }
+    )
+    gsap.fromTo(
+      text3.chars,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        opacity: 1,
+        stagger: 0.1,
+        y: 0,
+      }
+    )
+    gsap.fromTo(
+      shapeFuture.chars,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        opacity: 1,
+        stagger: 0.1,
+        y: 0,
+      }
+    );
 
-  tl.play();
 
-  // ScrollTrigger.create({
-  //   animation: tl,
-  //   trigger: ".section-1",
-  //   start: "top center",
-  //   scrub: 1,
-  // });
+  gsap.fromTo(
+    "#graph-icon",
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+      duration: ".7",
+    },
+    "-=.7"
+  );
+
+  gsap.fromTo(
+    "#comp-image",
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+      duration: ".7",
+    },
+    "-=.7"
+  );
+
+  gsap.fromTo(
+    "#music-icon",
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+    }
+  );
+
+ 
+  function createScrollTriggerAnimation(element, trigger, start) {
+    gsap.fromTo(
+      element,
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: trigger,
+          start: start,
+          toggleActions: "play pause resume reset",
+          markers: true,
+          ease: "power1.inOut",
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1.5,
+      }
+    );
+  }
+  
+  // Section 2 Animations
+  createScrollTriggerAnimation("#strategic-success", shapeFuture.words, "center center");
+  createScrollTriggerAnimation("#setting-reports", "#strategic-success", "top bottom");
+  
 });
